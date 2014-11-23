5301d407716d0e80b1c4d6e889d328495def57e4
davidnbresch <david.bresch@gmail.com>
2014-11-23 01:32:00 +0100

    cleanup
    in climada_xlsread, try/catch disabled to allow for better debugging.

--- code/climada_xlsread.m
+++ code/climada_xlsread.m
@@ -4,8 +4,10 @@ function res=climada_xlsread(interactive_mode,excel_file,in_excel_sheet,silent_m
 %   climada_xlsread
 % PURPOSE:
 %   read one Excel sheet from an Excel file and return the content in a
-%   structure. 
-%   
+%   structure.
+%
+%   Usually, just read each column of the Excel tab into one variable.
+%
 %   NOTE: for older MATLAB versions, only the array-type input works, not
 %   the single variables (header) etc.
 %
@@ -17,21 +19,21 @@ function res=climada_xlsread(interactive_mode,excel_file,in_excel_sheet,silent_m
 %       Any further cells to the right are not processed (eg used for comments,
 %       as shown below)
 %   the last row before the data section is used to label the data columns
-% 
+%
 %   an Excel sheet of eg the following form will be processed:
 %
-% %climada post event TC trackdata sheet, fill in at least the bold data, either MaxSustainedWind or Pressure?									
-% *event_date	20030906	the date as yyyymmdd							
-% *event_name	Isabel	    the name							
-% *MaxSustainedWindUnit	kn	either kn (also use kn for kt), mph, km/h or m/s							
-% *CentralPressureUnit	mb	mb or hPa							
-% *CelerityUnit	kn	        either kn, mph, km/h or m/s							
-% *track_timestep	6	    in hours, used in Celerity calculation if Celerity and/or time not given below							
-% *unique_ID	0	        default=0							
-% *MaxSaffSimp	4	        maximum Safir Simson scale reached							
-% *record_date	20030919	date of this record							
-% *comment	test	        free comment							
-% %follows the track data (next line are headers, fill in as much as you know, do NOT change headers!)									
+% %climada post event TC trackdata sheet, fill in at least the bold data, either MaxSustainedWind or Pressure?
+% *event_date	20030906	the date as yyyymmdd
+% *event_name	Isabel	    the name
+% *MaxSustainedWindUnit	kn	either kn (also use kn for kt), mph, km/h or m/s
+% *CentralPressureUnit	mb	mb or hPa
+% *CelerityUnit	kn	        either kn, mph, km/h or m/s
+% *track_timestep	6	    in hours, used in Celerity calculation if Celerity and/or time not given below
+% *unique_ID	0	        default=0
+% *MaxSaffSimp	4	        maximum Safir Simson scale reached
+% *record_date	20030919	date of this record
+% *comment	test	        free comment
+% %follows the track data (next line are headers, fill in as much as you know, do NOT change headers!)
 % lat	lon	                MaxSustainedWind	CentralPressure	Celerity	SaffSimp	time	yyyy	mm	dd
 % 14	-34	61.775			0	                                                            13	2003	9	6
 % 13.6	-34.5	61.775	    0	                                                            15	2003	9	6
@@ -136,153 +138,157 @@ if ~silent_mode,fprintf('reading sheet %s from %s\n',excel_sheet,excel_file);end
 
 % actually read the Excel sheet
 % -----------------------------
-try % the xlsread for later MATLAB versions, works also for complex Excel sheets
-    [NUMERIC,TEXT,RAW]=xlsread(excel_file,excel_sheet);
-    
-    % first, process all single variables
-    % -----------------------------------
-    warning off MATLAB:nonIntegerTruncatedInConversionToChar % to avoid WARNINGS
-    first_column=char(RAW{:,1}); % get first column
-    max_header_length=1;
-    for i=1:size(RAW,1)
-        if ischar(RAW{i,1}),max_header_length=i;end
-    end
-    
-    %second_column=char(RAW{1:max_header_length,2}); % get second column
-    single_pos=strmatch('*',first_column(1:max_header_length,1)); % get all *... values
-    for var_i=1:length(single_pos)
-        eff_pos=single_pos(var_i);
-        variable_name=first_column(eff_pos,2:end);
-        variable_value=RAW{eff_pos,2};
-        try
-            res = setfield(res,variable_name,variable_value); % add to struct
-            if ~ischar(variable_value)
-                variable_value_str=num2str(variable_value);
-            else
-                variable_value_str=variable_value;
-            end
-            if ~silent_mode,fprintf(' - assigning %s: %s\n',deblank(variable_name),variable_value_str);end
-        catch
-            if ~silent_mode, fprintf(' - WARN: assigning %s failed\n',variable_name);end
-        end
-    end
-    
-    % figure out the header line position
-    comment_pos=strmatch('%',first_column(1:max_header_length,1)); % get all *... values
-    if length(single_pos)==0,single_pos=0;end
-    if length(comment_pos)==0,comment_pos=single_pos;end
-    header_pos=max(max(comment_pos),max(single_pos))+1;
-    
-    % process the array data
-    % ----------------------
-    for header_i=1:size(RAW,2)
-        header_tag=char(RAW{header_pos,header_i});
-        % purge from any special characters
-        header_tag=strrep(header_tag,'(','');
-        header_tag=strrep(header_tag,')','');
-        header_tag=strrep(header_tag,'*','');
-        header_tag=strrep(header_tag,'&','');
-        header_tag=strrep(header_tag,'[','');
-        header_tag=strrep(header_tag,']','');
-        header_tag=strrep(header_tag,'?','');
-        %header_tag=strrep(header_tag,' ','');
-        header_tag=strrep(header_tag,'=','');
-        header_tag=strrep(header_tag,'+','');
-        header_tag=strrep(header_tag,'-','');
-        header_tag=strrep(header_tag,'%','');
-        header_tag=strrep(header_tag,'/','');
-        header_tag=strrep(header_tag,'{','');
-        header_tag=strrep(header_tag,'}','');
-        header_tag=strrep(header_tag,':','');
-        header_tag=strrep(header_tag,'.','');
-        header_tag=strrep(header_tag,' ','_');
-        header_tag=strrep(header_tag,'__','_');
-        header_tag=strrep(header_tag,'__','_');
-        header_tag=deblank(header_tag);
-        arr_values=[];
-        
-        if length(RAW{header_pos+1,header_i})>0
-            if ~silent_mode,fprintf(' - processing %s\n',header_tag);end
-            
-            if isfield(res,header_tag)
-                if ~silent_mode, fprintf('\tdouble entry for header %s (%i skipped)\n',header_tag, header_i);end
-            else
-                arr_values=RAW(header_pos+1:end,header_i);
-                if isnumeric([arr_values{:}]) %isnumeric(arr_values{1})
-                    temp=[];
-                    for element_i=1:size(arr_values,1)
-                        temp(element_i,1)=arr_values{element_i};
-                    end % element_i
-                    if ~silent_mode,fprintf('   -> converted to numeric\n');end
-                    arr_values=temp;
-                end
+%%try % TRY switched off, as the fallback is not very useful, hence this
+% way, debugging is easier (i.e. if one reads strange Excel sheets etc)
+% the xlsread for later MATLAB versions, works also for complex Excel sheets
+[NUMERIC,TEXT,RAW]=xlsread(excel_file,excel_sheet);
 
-                if length(header_tag) > 0
-                    % counter = 0;
-                    % while isfield(res,header_tag)
-                    %     %if ~silent_mode, fprintf('WARNING: (empty) header/column %i skipped\n',header_i);end
-                    %     counter       = counter+1;
-                    %     header_tag    = [header_tag '_' int2str(counter)];
-                    % end
-                    try   
-                        res = setfield(res,header_tag,arr_values); % add to struct
-                    catch
-                        if ~isnan(str2double(header_tag(1))), header_tag = ['Att_' header_tag];end% Attribute starts with number, rewrite for setfield
-                        header_tag = smat_removeBlanks({header_tag},'_'); % deblank attributes
-                        res = setfield(res,header_tag{:},arr_values); % add to struct
-                    end
-                else
-                    if ~silent_mode, fprintf('WARNING: (empty) header/column %i skipped\n',header_i);end
-                end
-            end %duplicate header_tag
+% first, process all single variables
+% -----------------------------------
+warning off MATLAB:nonIntegerTruncatedInConversionToChar % to avoid WARNINGS
+first_column=char(RAW{:,1}); % get first column
+max_header_length=1;
+for i=1:size(RAW,1)
+    if ischar(RAW{i,1}),max_header_length=i;end
+end
+
+%second_column=char(RAW{1:max_header_length,2}); % get second column
+single_pos=strmatch('*',first_column(1:max_header_length,1)); % get all *... values
+for var_i=1:length(single_pos)
+    eff_pos=single_pos(var_i);
+    variable_name=first_column(eff_pos,2:end);
+    variable_value=RAW{eff_pos,2};
+    try
+        res = setfield(res,variable_name,variable_value); % add to struct
+        if ~ischar(variable_value)
+            variable_value_str=num2str(variable_value);
         else
-            if ~silent_mode,fprintf(' - %s skipped\n',header_tag);end
+            variable_value_str=variable_value;
         end
+        if ~silent_mode,fprintf(' - assigning %s: %s\n',deblank(variable_name),variable_value_str);end
+    catch
+        if ~silent_mode, fprintf(' - WARN: assigning %s failed\n',variable_name);end
     end
+end
+
+% figure out the header line position
+comment_pos=strmatch('%',first_column(1:max_header_length,1)); % get all *... values
+if length(single_pos)==0,single_pos=0;end
+if length(comment_pos)==0,comment_pos=single_pos;end
+header_pos=max(max(comment_pos),max(single_pos))+1;
+
+% process the array data
+% ----------------------
+for header_i=1:size(RAW,2)
+    header_tag=char(RAW{header_pos,header_i});
+    % purge from any special characters
+    header_tag=strrep(header_tag,'(','');
+    header_tag=strrep(header_tag,')','');
+    header_tag=strrep(header_tag,'*','');
+    header_tag=strrep(header_tag,'&','');
+    header_tag=strrep(header_tag,'[','');
+    header_tag=strrep(header_tag,']','');
+    header_tag=strrep(header_tag,'?','');
+    %header_tag=strrep(header_tag,' ','');
+    header_tag=strrep(header_tag,'=','');
+    header_tag=strrep(header_tag,'+','');
+    header_tag=strrep(header_tag,'-','');
+    header_tag=strrep(header_tag,'%','');
+    header_tag=strrep(header_tag,'/','');
+    header_tag=strrep(header_tag,'{','');
+    header_tag=strrep(header_tag,'}','');
+    header_tag=strrep(header_tag,':','');
+    header_tag=strrep(header_tag,'.','');
+    header_tag=strrep(header_tag,',','');
+    header_tag=strrep(header_tag,' ','_');
+    header_tag=strrep(header_tag,'__','_');
+    header_tag=strrep(header_tag,'__','_');
+    header_tag=deblank(header_tag);
+    arr_values=[];
     
-catch % try
-    
-    fprintf('NOTE: simple xlsread\n');
-    [NUMERIC,TEXT]=xlsread(excel_file,excel_sheet);
-    
-    for tag_i=1:size(NUMERIC,2)
+    if length(RAW{header_pos+1,header_i})>0
+        if ~silent_mode,fprintf(' - processing %s\n',header_tag);end
         
-        header_tag = TEXT{1,tag_i};
-        % purge from any special characters
-        header_tag=strrep(header_tag,'(','');
-        header_tag=strrep(header_tag,')','');
-        header_tag=strrep(header_tag,'*','');
-        header_tag=strrep(header_tag,'&','');
-        header_tag=strrep(header_tag,'[','');
-        header_tag=strrep(header_tag,']','');
-        %header_tag=strrep(header_tag,' ','');
-        header_tag=strrep(header_tag,'?','');
-        header_tag=strrep(header_tag,'=','');
-        header_tag=strrep(header_tag,'+','');
-        header_tag=strrep(header_tag,'-','');
-        header_tag=strrep(header_tag,'%','');
-        header_tag=strrep(header_tag,'/','');
-        header_tag=strrep(header_tag,'{','');
-        header_tag=strrep(header_tag,'}','');
-        header_tag=strrep(header_tag,':','');
-        header_tag=strrep(header_tag,'.','');
-        header_tag=strrep(header_tag,' ','_');
-        header_tag=strrep(header_tag,'__','_');
-        header_tag=strrep(header_tag,'__','_');
-        
-        header_tag=deblank(header_tag);
-                
-        if ~isempty(NUMERIC)
-            fprintf('Column %d: %s\t', tag_i, header_tag)
-            if all(isnan(NUMERIC(:,tag_i)))
-                fprintf('No data available\n')
-            else 
-                res = setfield(res,header_tag,NUMERIC(:,tag_i)); %#ok<SFLD> % add to struct
-                fprintf('added to struct\n')
+        if isfield(res,header_tag)
+            if ~silent_mode, fprintf('\tdouble entry for header %s (%i skipped)\n',header_tag, header_i);end
+        else
+            arr_values=RAW(header_pos+1:end,header_i);
+            if isnumeric([arr_values{:}]) %isnumeric(arr_values{1})
+                temp=[];
+                for element_i=1:size(arr_values,1)
+                    temp(element_i,1)=arr_values{element_i};
+                end % element_i
+                if ~silent_mode,fprintf('   -> converted to numeric\n');end
+                arr_values=temp;
             end
-        end
+            
+            if length(header_tag) > 0
+                % counter = 0;
+                % while isfield(res,header_tag)
+                %     %if ~silent_mode, fprintf('WARNING: (empty) header/column %i skipped\n',header_i);end
+                %     counter       = counter+1;
+                %     header_tag    = [header_tag '_' int2str(counter)];
+                % end
+                try
+                    res = setfield(res,header_tag,arr_values); % add to struct
+                catch
+                    fprintf('consider simplyfying header tag %s\n',header_tag);
+                    if ~isnan(str2double(header_tag(1))), header_tag = ['Att_' header_tag];end% Attribute starts with number, rewrite for setfield
+                    header_tag = strrep(header_tag,'_'); % deblank attributes
+                    res = setfield(res,header_tag{:},arr_values); % add to struct
+                end
+            else
+                if ~silent_mode, fprintf('WARNING: (empty) header/column %i skipped\n',header_i);end
+            end
+        end %duplicate header_tag
+    else
+        if ~silent_mode,fprintf(' - %s skipped\n',header_tag);end
     end
-    
-end % try
+end
+
+% catch % try
+%
+%     fprintf('NOTE: simple xlsread\n');
+%     [NUMERIC,TEXT]=xlsread(excel_file,excel_sheet);
+%
+%     for tag_i=1:size(NUMERIC,2)
+%
+%         header_tag = TEXT{1,tag_i};
+%         % purge from any special characters
+%         header_tag=strrep(header_tag,'(','');
+%         header_tag=strrep(header_tag,')','');
+%         header_tag=strrep(header_tag,'*','');
+%         header_tag=strrep(header_tag,'&','');
+%         header_tag=strrep(header_tag,'[','');
+%         header_tag=strrep(header_tag,']','');
+%         %header_tag=strrep(header_tag,' ','');
+%         header_tag=strrep(header_tag,'?','');
+%         header_tag=strrep(header_tag,'=','');
+%         header_tag=strrep(header_tag,'+','');
+%         header_tag=strrep(header_tag,'-','');
+%         header_tag=strrep(header_tag,'%','');
+%         header_tag=strrep(header_tag,'/','');
+%         header_tag=strrep(header_tag,'{','');
+%         header_tag=strrep(header_tag,'}','');
+%         header_tag=strrep(header_tag,':','');
+%         header_tag=strrep(header_tag,'.','');
+%         header_tag=strrep(header_tag,' ','_');
+%         header_tag=strrep(header_tag,'__','_');
+%         header_tag=strrep(header_tag,'__','_');
+%
+%         header_tag=deblank(header_tag);
+%
+%         if ~isempty(NUMERIC)
+%             fprintf('Column %d: %s\t', tag_i, header_tag)
+%             if all(isnan(NUMERIC(:,tag_i)))
+%                 fprintf('No data available\n')
+%             else
+%                 res = setfield(res,header_tag,NUMERIC(:,tag_i)); %#ok<SFLD> % add to struct
+%                 fprintf('added to struct\n')
+%             end
+%         end
+%     end
+%
+% end % try
 
 return
