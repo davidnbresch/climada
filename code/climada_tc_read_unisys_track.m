c5bddab939d15f38a53434097c4dc373966fa48b
davidnbresch <david.bresch@gmail.com>
2015-10-18 16:05:08 +0200

    UNISYS reading routine improved
    skip double and backstopping entries in raw data track files

--- code/climada_tc_read_unisys_track.m
+++ code/climada_tc_read_unisys_track.m
@@ -3,10 +3,14 @@ function [tc_track,track_filename] = climada_tc_read_unisys_track(track_filename
 % NAME:
 %   climada_tc_read_unisys_track
 % PURPOSE:
-%   read a single track data file as downloaded from 
+%   read a single track data file as downloaded from
 %   http://www.weather.unisys.com/hurricane/
 %   can be used to add to existing tc_track structure like:
 %   tc_track(end+1)=climada_tc_read_unisys_track
+%
+%   Note that identical (double entry) and backward timesteps are removed.
+%   In such cases, you better inspect the tc_track file (source) and remove
+%   using an editor. 
 %
 %   See also e.g. climada_event_damage_data_tc
 % CALLING SEQUENCE:
@@ -25,6 +29,7 @@ function [tc_track,track_filename] = climada_tc_read_unisys_track(track_filename
 % David N. Bresch, david.bresch@gmail.com, 20110429
 % Lea Mueller, 20110718
 % David N. Bresch, david.bresch@gmail.com, 20150220, init_vars reset removed
+% David N. Bresch, david.bresch@gmail.com, 20151018, forecast file added to output and automatic removal of backward timesteps
 %-
 
 global climada_global
@@ -50,7 +55,7 @@ if isempty(track_filename)
         return % Cancel pressed
     else
         track_filename=[pathname filename];
-    end 
+    end
 end
 
 if exist(track_filename,'file')
@@ -78,7 +83,7 @@ if exist(track_filename,'file')
                 tc_track.mm   = 99;
             end
         end % Date
-
+        
         if findstr(one_line,'Hurricane');
             try
                 tc_track.name=strrep(strrep(one_line,'Hurricane',''),' ','');
@@ -110,7 +115,7 @@ if exist(track_filename,'file')
                 tc_track.name='NNN';
             end
         end % Tropical
-
+        
         if findstr(one_line,'*EyeDiameterFactor');
             try
                 tc_track.EyeDiameterFactor=str2num( strrep(strrep(one_line,'*EyeDiameterFactor=',''),' ','') );
@@ -121,7 +126,7 @@ if exist(track_filename,'file')
                 end
             end
         end % EyeDiameterFactor
-
+        
         % continue reading header until you get the ADV... line
         if findstr(one_line,'ADV') & ( findstr(one_line,'TIME') & findstr(one_line,'WIND'))
             continue_reading_header=0;
@@ -129,7 +134,7 @@ if exist(track_filename,'file')
     end % continue_reading_header
     
     % read data
-    % ---------    
+    % ---------
     %     1  14.00  -34.00 09/06/13Z   35  1005 TROPICAL STORM
     %     2  13.60  -34.50 09/06/15Z   35  1005 TROPICAL STORM
     %     3  13.40  -35.40 09/06/21Z   45  1000 TROPICAL STORM
@@ -142,67 +147,69 @@ if exist(track_filename,'file')
     %     10  17.20  -42.60 09/08/15Z  100   962 HURRICANE-3
     %     11  17.90  -43.70 09/08/21Z  110   952 HURRICANE-3
     %     12  18.50  -44.50 09/09/03Z  115   948 HURRICANE-4
-    %     
+    %
     node_i            = 1;
     use_pressure_data = 1;
     while not(feof(fid))
-        tc_track.forecast(node_i) = 0; % init
         one_line              = fgetl(fid);
-        [token,one_line]      = strtok(one_line); % split ADV
-        if findstr(token,'+'),tc_track.forecast(node_i)=1;end
-        
-        [token,one_line]      = strtok(one_line); % split LAT
-        tc_track.lat(node_i)  = str2num(token);
-        [token,one_line]      = strtok(one_line); % split LON
-        tc_track.lon(node_i)  = str2num(token);
-        [token,one_line]      = strtok(one_line); % split TIME
-        time_token            = token;
-    %        try
-        tc_track.mm(node_i)   = str2num(time_token(1:2)); % month
-        tc_track.dd(node_i)   = str2num(time_token(4:5)); % day
-        tc_track.hh(node_i)   = str2num(time_token(7:8)); % time (Z)
-        tc_track.yyyy(node_i) = track_year; % year
-                 
-        %         catch
-        %             % could be without month
-        %             fprintf('WARNING: track file does NOT contain month: %s (dummy month=1 used)\n',time_token);
-        %             tc_track.mm(node_i)=1; % just dummy month
-        %             tc_track.dd(node_i)=str2num(time_token(1:2)); % day
-        %             tc_track.time(node_i)=str2num(time_token(4:5)); % time (Z)
-        %             track_year
-        %             tc_track.yyyy(node_i)=track_year; % year
-        %         end
-                [token,one_line] = strtok(one_line); % split WIND
+        if length(one_line)>1
+            [token,one_line]      = strtok(one_line); % split ADV
+            tc_track.forecast(node_i) = 0; % init
+            if findstr(token,'+'),tc_track.forecast(node_i)=1;end
+            
+            [token,one_line]      = strtok(one_line); % split LAT
+            tc_track.lat(node_i)  = str2num(token);
+            [token,one_line]      = strtok(one_line); % split LON
+            tc_track.lon(node_i)  = str2num(token);
+            [token,one_line]      = strtok(one_line); % split TIME
+            time_token            = token;
+            %        try
+            tc_track.mm(node_i)   = str2num(time_token(1:2)); % month
+            tc_track.dd(node_i)   = str2num(time_token(4:5)); % day
+            tc_track.hh(node_i)   = str2num(time_token(7:8)); % time (Z)
+            tc_track.yyyy(node_i) = track_year; % year
+            
+            %         catch
+            %             % could be without month
+            %             fprintf('WARNING: track file does NOT contain month: %s (dummy month=1 used)\n',time_token);
+            %             tc_track.mm(node_i)=1; % just dummy month
+            %             tc_track.dd(node_i)=str2num(time_token(1:2)); % day
+            %             tc_track.time(node_i)=str2num(time_token(4:5)); % time (Z)
+            %             track_year
+            %             tc_track.yyyy(node_i)=track_year; % year
+            %         end
+            [token,one_line] = strtok(one_line); % split WIND
+            try
+                tc_track.MaxSustainedWind(node_i)=str2num(token);
+            catch
+                fprintf('WARNING: no valid MaxSustainedWind for %s\n',time_token);
+                tc_track.MaxSustainedWind(node_i)=0;
+            end
+            [token,one_line] = strtok(one_line); % split PR
+            try
+                tc_track.CentralPressure(node_i)=str2num(token);
+            catch
+                if use_pressure_data,fprintf('WARNING: %s, %s is (likely) a forecast timestep - pressure data thus not complete\n',track_filename,time_token);end
+                use_pressure_data=0;
+                tc_track.CentralPressure(node_i)=NaN;
+            end
+            [token,one_line] = strtok(one_line); % split STAT
+            token=upper(strrep(token,' ',''));
+            if strcmp(token,'TROPICAL')
+                tc_track.SaffSimp(node_i)=0;
+            else
                 try
-                    tc_track.MaxSustainedWind(node_i)=str2num(token);
+                    if strcmp(upper(deblank(token)),'SUPER'),token=one_line;end
+                    token=strrep(upper(token),'HURRICANE-','');
+                    token=strrep(upper(token),'TYPHOON-','');
+                    token=deblank(strrep(upper(token),'SUPER',''));
+                    tc_track.SaffSimp(node_i)=str2num(token);
                 catch
-                    fprintf('WARNING: no valid MaxSustainedWind for %s\n',time_token);
-                    tc_track.MaxSustainedWind(node_i)=0;
-                end
-                [token,one_line] = strtok(one_line); % split PR
-                try
-                    tc_track.CentralPressure(node_i)=str2num(token);
-                catch
-                    if use_pressure_data,fprintf('WARNING: %s, %s is (likely) a forecast timestep - pressure data thus not complete\n',track_filename,time_token);end
-                    use_pressure_data=0;
-                    tc_track.CentralPressure(node_i)=NaN;
-                end
-                [token,one_line] = strtok(one_line); % split STAT
-                token=upper(strrep(token,' ',''));
-                if strcmp(token,'TROPICAL')
                     tc_track.SaffSimp(node_i)=0;
-                else
-                    try
-                        if strcmp(upper(deblank(token)),'SUPER'),token=one_line;end
-                        token=strrep(upper(token),'HURRICANE-','');
-                        token=strrep(upper(token),'TYPHOON-','');
-                        token=deblank(strrep(upper(token),'SUPER',''));
-                        tc_track.SaffSimp(node_i)=str2num(token);
-                    catch
-                        tc_track.SaffSimp(node_i)=0;
-                    end
                 end
+            end            
             node_i=node_i+1; % increment
+        end % length(one_line)>1
     end
     fclose(fid);
     
@@ -210,8 +217,8 @@ if exist(track_filename,'file')
     if ~use_pressure_data && really_delete_pressure,tc_track=rmfield(tc_track,'CentralPressure');end
     
     %     track_timestep
-    % 
-    %     
+    %
+    %
     %     - processing time
     %     - processing yyyy
     %     - processing mm
@@ -240,7 +247,7 @@ if exist(track_filename,'file')
         tc_track.mm   = str2num(yyyymmddstr(5:6));
         tc_track.dd   = str2num(yyyymmddstr(7:8));
     end
-
+    
 else
     fprintf('ERROR: track data file %s does not exist\n',track_filename);
     return
@@ -257,26 +264,74 @@ tc_track.datenum = datenum(tc_track.yyyy,tc_track.mm,tc_track.dd,tc_track.hh,0,0
 
 % find and delete duplicate entries
 index_sametime = find(diff(tc_track.datenum)==0)+1;
+if sum(index_sametime)>0
+    tc_track.lon             (index_sametime) = [];
+    tc_track.lat             (index_sametime) = [];
+    tc_track.MaxSustainedWind(index_sametime) = [];
+    tc_track.CentralPressure (index_sametime) = [];
+    tc_track.yyyy            (index_sametime) = [];
+    tc_track.mm              (index_sametime) = [];
+    tc_track.dd              (index_sametime) = [];
+    tc_track.hh              (index_sametime) = [];
+    tc_track.datenum         (index_sametime) = [];
+    tc_track.forecast        (index_sametime) = [];
+    fprintf('Warning: %i duplicate timestep(s) removed\n',length(index_sametime));
+end
 
-tc_track.lon             (index_sametime) = [];
-tc_track.lat             (index_sametime) = [];
-tc_track.MaxSustainedWind(index_sametime) = [];
-tc_track.CentralPressure (index_sametime) = [];
-tc_track.yyyy            (index_sametime) = [];
-tc_track.mm              (index_sametime) = [];
-tc_track.dd              (index_sametime) = [];
-tc_track.hh              (index_sametime) = [];
-tc_track.datenum    (index_sametime) = [];
+% find and delete entries with previous times
+index_sametime = find(diff(tc_track.datenum)<0)+1;
+if sum(index_sametime)>0
+    tc_track.lon             (index_sametime) = [];
+    tc_track.lat             (index_sametime) = [];
+    tc_track.MaxSustainedWind(index_sametime) = [];
+    tc_track.CentralPressure (index_sametime) = [];
+    tc_track.yyyy            (index_sametime) = [];
+    tc_track.mm              (index_sametime) = [];
+    tc_track.dd              (index_sametime) = [];
+    tc_track.hh              (index_sametime) = [];
+    tc_track.datenum         (index_sametime) = [];
+    tc_track.forecast        (index_sametime) = [];
+    fprintf('Warning: %i backward timestep(s) removed\n',length(index_sametime));
+end
 
-% calculate timestep between measurement and the following measurement in
-% hours
+% find and delete entries with previous times (and a second time, to be sure)
+index_sametime = find(diff(tc_track.datenum)<0)+1;
+if sum(index_sametime)>0
+    tc_track.lon             (index_sametime) = [];
+    tc_track.lat             (index_sametime) = [];
+    tc_track.MaxSustainedWind(index_sametime) = [];
+    tc_track.CentralPressure (index_sametime) = [];
+    tc_track.yyyy            (index_sametime) = [];
+    tc_track.mm              (index_sametime) = [];
+    tc_track.dd              (index_sametime) = [];
+    tc_track.hh              (index_sametime) = [];
+    tc_track.datenum         (index_sametime) = [];
+    tc_track.forecast        (index_sametime) = [];
+    fprintf('Warning: %i additional backward timestep(s) removed\n',length(index_sametime));
+end
+
+% find and delete duplicate entries
+index_sametime = find(diff(tc_track.datenum)==0)+1;
+if sum(index_sametime)>0
+    tc_track.lon             (index_sametime) = [];
+    tc_track.lat             (index_sametime) = [];
+    tc_track.MaxSustainedWind(index_sametime) = [];
+    tc_track.CentralPressure (index_sametime) = [];
+    tc_track.yyyy            (index_sametime) = [];
+    tc_track.mm              (index_sametime) = [];
+    tc_track.dd              (index_sametime) = [];
+    tc_track.hh              (index_sametime) = [];
+    tc_track.datenum         (index_sametime) = [];
+    tc_track.forecast        (index_sametime) = [];
+    fprintf('Warning: %i additional duplicate timestep(s) removed\n',length(index_sametime));
+end
+
+% calculate timestep between measurement and the following measurement in hours
 for timestep_i = 1:length(tc_track.datenum)-1
     tc_track.TimeStep(timestep_i) = (tc_track.datenum(timestep_i+1) - tc_track.datenum(timestep_i))*24; %[h]
 end
 tc_track.TimeStep(length(tc_track.datenum)) = tc_track.TimeStep(length(tc_track.datenum)-1);
 %------
-  
-
 
 % to convert to exact same order as in other climada routines:
 tc_track_out.MaxSustainedWindUnit = tc_track.MaxSustainedWindUnit;
@@ -292,9 +347,10 @@ tc_track_out.dd                   = tc_track.dd;
 tc_track_out.hh                   = tc_track.hh;
 tc_track_out.ID_no                = tc_track.ID_no;
 tc_track_out.orig_event_flag      = 1;
-tc_track_out.datenum         = tc_track.datenum;
+tc_track_out.datenum              = tc_track.datenum;
 tc_track_out.extratrop            = ''; % dummy
 tc_track_out.name                 = tc_track.name;
+tc_track_out.forecast             = tc_track.forecast; % added 20151018
 
 tc_track = []; % reset
 tc_track = tc_track_out;
@@ -303,6 +359,4 @@ if check_plot
     plot(tc_track.lon,tc_track.lat);hold on
     climada_plot_world_borders;
 end
-
-return
 
