2c660265a82f312cf4580bbda7e50d05c518c1ce
muellele <muellele@gmail.com>
2015-10-21 15:57:25 +0200

    Add intensity shift
      - in climada_hazard_clim_scen.m

--- code/climada_hazard_clim_scen.m
+++ code/climada_hazard_clim_scen.m
@@ -1,4 +1,4 @@
-function hazard = climada_hazard_clim_scen(hazard,hazard_clim_file,frequency_screw,intensity_screw)
+function hazard = climada_hazard_clim_scen(hazard,hazard_clim_file,frequency_screw,intensity_screw,intensity_shift)
 % climada
 % NAME:
 %   climada_template
@@ -27,11 +27,12 @@ function hazard = climada_hazard_clim_scen(hazard,hazard_clim_file,frequency_scr
 % Lea Mueller, 20110720
 % Reto Stockmann 20120719
 % David N. Bresch, david.bresch@gmail.com, 20150906, allow for name without path on input
+% Lea Mueller, muellele@gmail.com, 20151021, add intensity shift
 %-
 
-% SAFETY message prior to first call - user is asked to comment the return statement
-fprintf('!!!   Modifications done in code, so please visit/edit the code prior to use\n');
-%%return % comment out, but activate once code is subject to use by newcomer
+% % SAFETY message prior to first call - user is asked to comment the return statement
+% fprintf('!!!   Modifications done in code, so please visit/edit the code prior to use\n');
+% %%return % comment out, but activate once code is subject to use by newcomer
 
 global climada_global
 if ~climada_init_vars,return;end % init/import global variables
@@ -43,6 +44,7 @@ if ~exist('hazard','var'),hazard=[];end
 if ~exist('hazard_clim_file','var'),hazard_clim_file=[];end
 if ~exist('frequency_screw','var'),frequency_screw=[];end
 if ~exist('intensity_screw','var'),intensity_screw=[];end
+if ~exist('intensity_shift','var'),intensity_shift=[];end
 
 % PARAMETERS
 %
@@ -57,6 +59,12 @@ end
 if isempty(intensity_screw)
     intensity_screw = 1.05;
 end
+% new hazard intensity=orig hazard intensity + intensity_shift
+% =0.0 for identity
+if isempty(intensity_shift)
+    intensity_shift = 0;
+end
+
 % define the reference year for this hazard set
 % default for future or scenario hazard is normally 2030
 hazard_reference_year = climada_global.future_reference_year;
@@ -87,7 +95,15 @@ hazard=climada_hazard2octave(hazard); % Octave compatibility for -v7.3 mat-files
 hazard.frequency = hazard.frequency*frequency_screw;
 
 % assumption 2) intensity increase
-hazard.intensity       = hazard.intensity*intensity_screw;
+hazard.intensity = hazard.intensity*intensity_screw;
+
+% assumption 3) intensity shift for nonzero elements
+if intensity_shift~= 0
+    [i,j,values] = find(hazard.intensity);
+    [m,n] = size(hazard.intensity);
+    hazard.intensity= sparse(i,j,values+intensity_shift,m,n);
+end
+
 
 pause(1)
 
@@ -121,6 +137,9 @@ hazard.date                    = datestr(now);
 
 save(hazard_clim_file,'hazard')
 
-fprintf('\n***Climate change scenario *** \n  intensity screw = %10.2f \n  frequency_screw = %10.2f \nsaved in \n%s \n\n', intensity_screw, frequency_screw,[climada_global.data_dir hazard_clim_file])
+hazard_units = '';
+if isfield(hazard,'units'),  hazard_units = hazard.units; end
+fprintf('Apply climate change scenario (%d)\n \t-frequency_screw = %10.4f \n \t-intensity screw = %10.4f \n \t-intensity shift = %10.4f %s \nsaved in \n%s \n\n',...
+    climada_global.future_reference_year,frequency_screw,intensity_screw,intensity_shift,hazard_units,[climada_global.data_dir hazard_clim_file])
 
 return
