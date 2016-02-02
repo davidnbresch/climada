53550a34c1a21d368f2a6af5f2d8fc6cffafaab1
davidnbresch <david.bresch@gmail.com>
2016-02-02 17:31:41 +0100

    EDS calc cleaned up

--- code/helper_functions/climada_entity_load.m
+++ code/helper_functions/climada_entity_load.m
@@ -1,4 +1,4 @@
-function entity=climada_entity_load(entity_file)
+function entity=climada_entity_load(entity)
 % climada
 % NAME:
 %   climada_entity_load
@@ -6,16 +6,16 @@ function entity=climada_entity_load(entity_file)
 %   load a previously saved entity (just to avoid typing long paths and
 %   filenames in the command window)
 % CALLING SEQUENCE:
-%   entity_out=climada_entity_load(entity_file)
+%   entity_out=climada_entity_load(entity)
 % EXAMPLE:
-%   entity=climada_entity_load(entity_file)
 %   entity=climada_entity_load('demo_today')
 % INPUTS:
-%   entity_file: the filename (with path, optional) of a previously saved
-%       entity, see climada_entity_read
-%       If no path provided, default path ../data/entities is used (and
-%       name can be without extension .mat)
-%       > promted for if not given
+%   hazard: the filename (and path, optional) of a previously saved entity
+%       structure. If no path provided, default path ../data/entities is used
+%       (and name can be without extension .mat)
+%       > promted for if empty
+%       OR: an entity structure, in which cas it is just returned (to allow
+%       calling climada_entity_load anytime, see e.g. climada_EDS_calc)
 % OPTIONAL INPUT PARAMETERS:
 % OUTPUTS:
 %   entity_out: a struct, see e.g. climada_assets_read for details
@@ -26,22 +26,24 @@ function entity=climada_entity_load(entity_file)
 % Lea Mueller, muellele@gmail.com, 20151124, check that field .assets exist
 % Lea Mueller, muellele@gmail.com, 20151127, enhance to work with complete entity as input
 % Lea Mueller, muellele@gmail.com, 20151127, set entity_file to empty if a struct without .assets
+% David N. Bresch, david.bresch@gmail.com, 20160202, speedup if entity structure passed
 %-
-
-entity=[]; % init output
 
 global climada_global
 if ~climada_init_vars,return;end % init/import global variables
 
 % poor man's version to check arguments
-if ~exist('entity_file','var'),entity_file=[];end
+if ~exist('entity','var'),entity=[];end
 
 % PARAMETERS
-%
 
-% if already a complete entity, return
-if isfield(entity_file,'assets'), entity = entity_file; return, end
-if isstruct(entity_file), entity_file = ''; end
+% if already a complete hazard, return
+if isstruct(entity)
+    return % already a hazard
+else
+    entity_file=entity;entity=[];
+    % from now on, entity_file is the input and entity will be output
+end
 
 % prompt for entity_file if not given
 if isempty(entity_file) % local GUI
@@ -56,9 +58,16 @@ end
 
 % complete path, if missing
 [fP,fN,fE]=fileparts(entity_file);
-if isempty(fP),entity_file=[climada_global.data_dir filesep 'entities' filesep fN fE];end
+if isempty(fP),fP=[climada_global.data_dir filesep 'entities'];end
+if isempty(fE),fE='.mat';end
+entity_file=[fP filesep fN fE];
 
-load(entity_file); % contains entity, the only line that really matters ;-)
+if ~exist(entity_file,'file')
+    fprintf('ERROR: entity does not exist %s\n',entity_file);
+    return
+else
+    load(entity_file); % contains entity, the only line that really matters ;-)
+end
 
 % check for valid/correct entity.assets.filename
 if isfield(entity,'assets')
--- code/helper_functions/climada_hazard_load.m
+++ code/helper_functions/climada_hazard_load.m
@@ -1,19 +1,21 @@
-function hazard=climada_hazard_load(hazard_file)
+function hazard=climada_hazard_load(hazard)
 % climada
 % NAME:
 %   climada_hazard_load
 % PURPOSE:
 %   load a hazard event set (just to avoid typing long paths and
-%   filenames in the cmd window)
+%   filenames in the command window)
 % CALLING SEQUENCE:
-%   hazard=climada_hazard_load(hazard_file)
+%   hazard=climada_hazard_load(hazard)
 % EXAMPLE:
-%   hazard=climada_hazard_load(hazard_file)
+%   hazard=climada_hazard_load(hazard)
 % INPUTS:
-%   hazard_file: the filename (and path, optional) of a previously saved hazard event set
-%       If no path provided, default path ../data/hazards is used (and name
-%       can be without extension .mat) 
-%       > promted for if not given
+%   hazard: the filename (and path, optional) of a previously saved hazard
+%       event set. If no path provided, default path ../data/hazards is used
+%       (and name can be without extension .mat)  
+%       > promted for if empty
+%       OR: a hazard structure, in which cas it is just returned (to allow
+%       calling climada_hazard_load anytime, see e.g. climada_EDS_calc)
 % OPTIONAL INPUT PARAMETERS:
 % OUTPUTS:
 %   hazard: a struct, see e.g. climada_tc_hazard_set
@@ -23,22 +25,24 @@ function hazard=climada_hazard_load(hazard_file)
 % David N. Bresch, david.bresch@gmail.com, 20150820, check for correct filename
 % Lea Mueller, muellele@gmail.com, 20151127, enhance to work with complete hazard as input
 % Lea Mueller, muellele@gmail.com, 20151127, set hazard_file to empty if a struct without .lon
+% David N. Bresch, david.bresch@gmail.com, 20160202, speedup if hazard structure passed
 %-
-
-hazard=[]; % init output
 
 global climada_global
 if ~climada_init_vars,return;end % init/import global variables
 
 % poor man's version to check arguments
-if ~exist('hazard_file','var'),hazard_file=[];end
+if ~exist('hazard','var'),hazard=[];end
 
 % PARAMETERS
 
 % if already a complete hazard, return
-if isfield(hazard_file,'lon'), hazard = hazard_file; return, end
-if isstruct(hazard_file), hazard_file = ''; end
-
+if isstruct(hazard)
+    return % already a hazard
+else
+    hazard_file=hazard;hazard=[];
+    % from now on, hazard_file is the input and hazard will be output
+end
 
 % prompt for hazard_file if not given
 if isempty(hazard_file) % local GUI
@@ -53,9 +57,16 @@ end
 
 % complete path, if missing
 [fP,fN,fE]=fileparts(hazard_file);
-if isempty(fP),hazard_file=[climada_global.data_dir filesep 'hazards' filesep fN fE];end
+if isempty(fP),fP=[climada_global.data_dir filesep 'hazards'];end
+if isempty(fE),fE='.mat';end
+hazard_file=[fP filesep fN fE];
 
-load(hazard_file);  % contains hazard, the only line that really matters ;-)
+if ~exist(hazard_file,'file')
+    fprintf('ERROR: hazard does not exist %s\n',hazard_file);
+    return
+else
+    load(hazard_file); % contains hazard, the only line that really matters ;-)
+end
 
 % check for valid/correct hazard.filename
 if ~strcmp(hazard_file,hazard.filename)
@@ -63,5 +74,4 @@ if ~strcmp(hazard_file,hazard.filename)
     save(hazard_file,'hazard')
 end
 
-return
-
+end % climada_hazard_load
