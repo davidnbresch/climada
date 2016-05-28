ec73a31df4477c3885fb5899edccff26ba60aeab
davidnbresch <david.bresch@gmail.com>
2016-05-28 13:10:06 +0200

    some small fixes
    tc_track_info to allow for (single) track selection
    event_damage_data_tc fast version of windfield added as option (needs
    edit in code)

--- code/helper_functions/climada_event_damage_data_tc.m
+++ code/helper_functions/climada_event_damage_data_tc.m
@@ -389,6 +389,7 @@ for step_i=min_node:max_node
         res=climada_tc_windfield(tc_track_segment,centroids,0,1,0);
     else
         res=climada_tc_windfield(tc_track_segment,centroids,0,-1,0);
+        %res=climada_tc_windfield_fast(tc_track_segment,centroids,0,-1,0);
     end
     
     hazard.intensity(step_i-min_node+1,:)=res.gust;
